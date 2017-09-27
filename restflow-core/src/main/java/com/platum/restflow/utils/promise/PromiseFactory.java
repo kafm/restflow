7db860a874735b0d4589f832d13372b390ef50bf
Kevin Martins <kevinmartins@MacBook-Pro-de-Kevin.local>
2017-09-27 15:15:33 -0100

    Save Point

--- restflow-core/src/main/java/com/platum/restflow/utils/promise/PromiseFactory.java
+++ restflow-core/src/main/java/com/platum/restflow/utils/promise/PromiseFactory.java
@@ -1,5 +1,10 @@
 package com.platum.restflow.utils.promise;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+
 import com.platum.restflow.utils.promise.impl.PromiseImpl;
 
 import io.vertx.core.Future;
@@ -13,5 +18,25 @@ public class PromiseFactory {
 	
 	public static <T> Promise<T> getPromiseInstance(Future<T> future) {
 		return new PromiseImpl<T>(future);
+	}
+	
+	public static Promise<List<PromiseResult<?>>> whenAll(List<Promise<?>> promises) {
+		Promise<List<PromiseResult<?>>> promise = getPromiseInstance();
+		try {
+			Validate.notEmpty(promises, "Promise list is empty.");
+			int expectedRes = promises.size();
+			List<PromiseResult<?>> results = new ArrayList<>();
+			promises.stream().forEach(p -> {
+				p.allways(h -> {
+					results.add(h);
+					if(results.size() == expectedRes) {
+						promise.resolve(results);
+					}
+				});
+			});			
+		} catch(Throwable e) {
+			promise.reject(e);
+		}
+		return promise;
 	}
 }
