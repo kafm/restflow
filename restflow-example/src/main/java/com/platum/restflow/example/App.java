3cf74f83c34c7329c684480917edcd804211e1dd
Ayrton Gomes <com.ayrton@gmail.com>
2018-10-30 11:18:28 -0100

    restflow-example basic proto is ready.

--- restflow-example/src/main/java/com/platum/restflow/example/App.java
+++ restflow-example/src/main/java/com/platum/restflow/example/App.java
@@ -1,13 +1,26 @@
 package com.platum.restflow.example;
 
-import com.platum.restflow.RestflowLauncher;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import com.platum.restflow.RestflowLauncher;
+import org.h2.Driver;
 /**
  * Hello world!
  */
 public class App  {
-    public static void main(String[] args) {
+	public final static String PRODUCT_TABLE = "CREATE TABLE product ("+ 
+		" id INT PRIMARY KEY,"+ 
+		" name VARCHAR(255)"+ 
+	")";
+
+	public static void main(String[] args) {
     	RestflowLauncher app = new RestflowLauncher();
-    	app.launch(args);
+    	app.launch(args)
+    	.success(v -> {
+    	});
+
     }
 }
